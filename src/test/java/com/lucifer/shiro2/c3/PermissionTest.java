d893cae5613683613c47675466d7f95b88a1e3b7
LuciferTM <royqian1988@gmail.com>
2017-12-21 16:48:32 +0800

    Permission

--- src/test/java/com/lucifer/shiro2/c3/PermissionTest.java
+++ src/test/java/com/lucifer/shiro2/c3/PermissionTest.java
@@ -1,5 +1,10 @@
 package com.lucifer.shiro2.c3;
 
+import org.apache.shiro.authz.UnauthorizedException;
+import org.apache.shiro.authz.permission.WildcardPermission;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * <p>Title:</p>
  * <p>Description: </p>
@@ -8,5 +13,100 @@ package com.lucifer.shiro2.c3;
  * @author Lucifer
  * @date 2017/12/21
  */
-public class PermissionTest {
+
+/**
+ * 8、性能问题
+
+ 通配符匹配方式比字符串相等匹配来说是更复杂的，因此需要花费更长时间，
+ 但是一般系统的权限不会太多，且可以配合缓存来提供其性能，
+ 如果这样性能还达不到要求我们可以实现位操作算法实现性能更好的权限匹配。
+ 另外实例级别的权限验证如果数据量太大也不建议使用，可能造成查询权限及匹配变慢。
+ 可以考虑比如在sql查询时加上权限字符串之类的方式在查询时就完成了权限匹配。
+ * */
+public class PermissionTest extends BaseTest{
+    @Test
+    public void testIsPermitted() {
+        login("classpath:c3/shiro-permission.ini", "zhang", "123");
+        //判断拥有权限：user:create
+        Assert.assertTrue(subject().isPermitted("user:create"));
+        //判断拥有权限：user:update and user:delete
+        Assert.assertTrue(subject().isPermittedAll("user:update", "user:delete"));
+        //判断没有权限：user:view
+        Assert.assertFalse(subject().isPermitted("user:view"));
+    }
+
+    /**
+     * check** 失败的情况下会抛出UnauthorizedException异常。
+     */
+
+    @Test(expected = UnauthorizedException.class)
+    public void testCheckPermission() {
+        login("classpath:c3/shiro-permission.ini", "zhang", "123");
+        //断言拥有权限：user:create
+        subject().checkPermission("user:create");
+        //断言拥有权限：user:delete and user:update
+        subject().checkPermissions("user:delete", "user:update");
+        //断言拥有权限：user:view 失败抛出异常
+        subject().checkPermissions("user:view");
+    }
+
+
+    @Test
+    public void testWildcardPermission1() {
+        login("classpath:c3/shiro-permission.ini", "li", "123");
+
+        subject().checkPermissions("system:user:update", "system:user:delete");
+        subject().checkPermissions("system:user:update,delete");
+    }
+
+    @Test
+    public void testWildcardPermission2() {
+        login("classpath:c3/shiro-permission.ini", "li", "123");
+        subject().checkPermissions("system:user:create,delete,update:view");
+
+        subject().checkPermissions("system:user:*");
+        subject().checkPermissions("system:user");
+    }
+
+    @Test
+    public void testWildcardPermission3() {
+        login("classpath:c3/shiro-permission.ini", "li", "123");
+        subject().checkPermissions("user:view");
+
+        subject().checkPermissions("system:user:view");
+    }
+
+    @Test
+    public void testWildcardPermission4() {
+        login("classpath:c3/shiro-permission.ini", "li", "123");
+        subject().checkPermissions("user:view:1");
+
+        subject().checkPermissions("user:delete,update:1");
+        subject().checkPermissions("user:update:1", "user:delete:1");
+
+        subject().checkPermissions("user:update:1", "user:delete:1", "user:view:1");
+
+        subject().checkPermissions("user:auth:1", "user:auth:2");
+
+    }
+
+    @Test
+    public void testWildcardPermission5() {
+        login("classpath:c3/shiro-permission.ini", "li", "123");
+        subject().checkPermissions("menu:view:1");
+
+        subject().checkPermissions("organization");
+        subject().checkPermissions("organization:view");
+        subject().checkPermissions("organization:view:1");
+
+    }
+
+
+    @Test
+    public void testWildcardPermission6() {
+        login("classpath:c3/shiro-permission.ini", "li", "123");
+        subject().checkPermission("menu:view:1");
+        subject().checkPermission(new WildcardPermission("menu:view:1"));
+
+    }
 }
