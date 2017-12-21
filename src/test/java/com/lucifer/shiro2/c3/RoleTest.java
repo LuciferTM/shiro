b343dcff1f7a0ff535c89956e2093b41ba468e67
LuciferTM <royqian1988@gmail.com>
2017-12-21 15:35:48 +0800

    RoleTest

---
+++ src/test/java/com/lucifer/shiro2/c3/RoleTest.java
@@ -0,0 +1,40 @@
+package com.lucifer.shiro2.c3;
+
+import org.apache.shiro.authz.UnauthorizedException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * <p>Title:</p>
+ * <p>Description: </p>
+ * <p>Company:</p>
+ *
+ * @author Lucifer
+ * @date 2017/12/21
+ */
+public class RoleTest extends BaseTest {
+    @Test
+    public void testHasRole() {
+        login("classpath:c3/shiro-role.ini", "zhang", "123");
+        //判断拥有角色：role1
+        Assert.assertTrue(subject().hasRole("role1"));
+        //判断拥有角色：role1 and role2
+        Assert.assertTrue(subject().hasAllRoles(Arrays.asList("role1", "role2")));
+        //判断拥有角色：role1 and role2 and !role3
+        boolean[] result = subject().hasRoles(Arrays.asList("role1", "role2", "role3"));
+        Assert.assertEquals(true, result[0]);
+        Assert.assertEquals(true, result[1]);
+        Assert.assertEquals(false, result[2]);
+    }
+
+    @Test(expected = UnauthorizedException.class)
+    public void testCheckRole() {
+        login("classpath:c3/shiro-role.ini", "zhang", "123");
+        //断言拥有角色：role1
+        subject().checkRole("role1");
+        //断言拥有角色：role1 and role3 失败抛出异常
+        subject().checkRoles("role1", "role3");
+    }
+}
